b9d40ddcfb9e8f142c5d623b1fa67c37a6f15902
ravisharmaa <bastola.ravi@gmail.com>
2020-01-31 08:58:54 +0545

    add port upto floor

--- panchanga.php
+++ panchanga.php
@@ -1,27 +1,57 @@
 <?php
+//CONSTANTS
 
-//port of sub numeric_p 
-function numeric_p($input) {
-	if preg_match('/^-?\d*\.?\d*(e\d|e-\d)?\d*$/', $input) {
+CONST PI_2 = pi()*2;
+CONST RAD = deg2rad(180/pi());
+
+//port of sub numeric_p
+function numericPort($input) {
+	if (preg_match('/^-?\d*\.?\d*(e\d|e-\d)?\d*$/', $input)) {
 		return true;
-	} 
+	}
 
 	return false;
 }
 
 
 //port of int_p
-
-function int_p($input) {
-	if ((numeric_p($input) && is_int($input)) {
+function intPort($input) {
+	if ((numeric_port($input) && is_int($input))) {
 		return true;
-	} 
+	}
 
 	return false;
 }
 
 //port of trunc
-
-function trunc($variable) {
-	
+function truncPort($input) {
+    return (int) $input;
 }
+
+//port of floor
+function floorPort($input) {
+    $y = (int)$input;
+
+    if ($input < $y) {
+        return $y - 1;
+    }
+
+    return $y;
+}
+
+//port of frac
+function fracPort($input) {
+    return $x - (int)$input;
+}
+
+//port of round
+function roundPort($input) {
+    return floorPort($input + 0.5);
+}
+
+//port of
+function sqrtPort($input) {
+    return $input * $input;
+}
+
+
